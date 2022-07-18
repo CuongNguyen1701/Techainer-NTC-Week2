80e3997891e6139e45f579db877fb4dccc93735a
CuongNguyen1701 <ntc170103@gmail.com>
2022-07-19 01:04:27 +0700

    fixed make it modular

--- learnyounode/make-it-modular.js
+++ learnyounode/make-it-modular.js
@@ -4,7 +4,6 @@ for (let i = 0; i < (process.argv.length - 2); i++) {
     arg[i] = process.argv[i+2];
 }
 let filtered = [];
-filtered = mymodule(arg[0],arg[1],(err,list) => {if(err) throw err; return list;});
-for (let i = 0; i < filtered.length; i++) {
-    console.log(filtered[i]);
-}
+filtered = mymodule(arg[0],arg[1],(err,list) => {
+    if(err) throw err; 
+    return list});
--- learnyounode/mymodule.js
+++ learnyounode/mymodule.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
-module.exports = async function (dir, extension, callback){
-    let filtered = []
-    const completed = await fs.readdir(dir, 'utf8' ,(err, list) => {
-        if(err) return callback(err);
-        filtered = list.filter(file => {
-            return file.endsWith(`.${extension}`);
-        });
-        console.log(list);
-        console.log(`filtered: ${filtered}`);
-        callback(null,filtered);
+module.exports = function (dir, extension, callback){
+    let filtered = [];
+    fs.readdir(dir, 'utf8' ,(err, list) => {
+    if(err) return callback(err);
+    list = list.filter(file => {
+        return file.endsWith(`.${extension}`);
+    });
+    filtered = list;
+    for (let i = 0; i < filtered.length; i++) {
+        console.log(filtered[i]);
+    }
+    return callback(null,list);
     })
-    // .then(()=>{
-    // })
-    console.log(`filtered: ${filtered}`);
-    return filtered;
-    
-
 
 }
