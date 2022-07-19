063d1a2db8bb99937925f44f751bf328944572aa
CuongNguyen1701 <ntc170103@gmail.com>
2022-07-19 09:25:08 +0700

    add http-collect.js

--- learnyounode/http-collect.js
+++ learnyounode/http-collect.js
@@ -1,15 +1,22 @@
 const http = require('http');
+const util = require('util');
 let arg = [];
 for (let i = 0; i < (process.argv.length - 2); i++) {
     arg[i] = process.argv[i+2];
 }
+
 http.get(arg[0], response =>{
     response.setEncoding('utf8');
-
+    let output = '';
+    let charNum = 0;
     response.on('data', chunk => {
-        console.log(chunk);
-     })
-     response.on('end', () =>{ })
+            output += chunk;
+    })
+    response.on('end', () =>{ 
+        charNum += output.length;
+        console.log(charNum);
+        console.log(output);
+    })
 }).on('error', err => {
     throw err;
 })
